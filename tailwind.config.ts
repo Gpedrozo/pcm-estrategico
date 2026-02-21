5a129bf5ad9ab6870fc95f0663144f85eba6b817
Gpedrozo <163456701+Gpedrozo@users.noreply.github.com>
2026-02-20 22:25:04 -0300

    Update tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -112,3 +112,16 @@ export default {
   },
   plugins: [require("tailwindcss-animate")],
 } satisfies Config;
+theme: {
+  extend: {
+    keyframes: {
+      fadeIn: {
+        '0%': { opacity: '0', transform: 'scale(0.9)' },
+        '100%': { opacity: '1', transform: 'scale(1)' },
+      },
+    },
+    animation: {
+      fadeIn: 'fadeIn 1.5s ease-in-out',
+    },
+  },
+},
