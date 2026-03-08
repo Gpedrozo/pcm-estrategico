efc96b8a65985c85733a5edd0aead24e995301fd
Gustavo Pedrozo <pedrozo@gppis.com.br>
2026-03-08 17:01:59 -0300

    test: alinhar arquivos owner ao portal consolidado

--- src/test/arquivos-owner.test.ts
+++ src/test/arquivos-owner.test.ts
@@ -4,7 +4,7 @@ import { OWNER_FILES } from "@/pages/ArquivosOwner";
 describe("ArquivosOwner", () => {
   it("deve expor os caminhos dos arquivos do owner para administração", () => {
     expect(OWNER_FILES.map((item) => item.path)).toEqual([
-      "/master-ti",
+      "/",
       "/usuarios",
       "/auditoria",
     ]);
